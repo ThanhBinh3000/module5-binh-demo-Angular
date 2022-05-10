40f082b08dc3cd69de250a1f951714085d531f9e
daothannhangian <binhk4a@ gmail.com>
2022-05-10 14:21:45 +0700

    jwt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './helper/auth-guard';
 
 
 const routes: Routes = [
   {
     path: 'product',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./product/product.module').then(module => module.ProductModule)
   },
   {
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../service/authentication/authentication.service';
+import {UserToken} from '../../model/user-token';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
+  currentUser: UserToken = {};
+  constructor(private authenticationService: AuthenticationService) {
+    this.currentUser = this.authenticationService.currentUserValue;
+  }
   ngOnInit() {
   }
 
